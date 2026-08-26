067f838eef85a72da29f6ee5aedc63d3234b3009
thalath <tunthalath@gmail.com>
2026-08-26 15:11:09 +0700

    app modified

--- app/__init__.py
+++ app/__init__.py
@@ -16,11 +16,20 @@ def create_app(class_type: type[Config] = Config):
     
     from app.routes.user_routes import user_bp
     from app.routes.auth_routes import auth_bp
+    from app.routes.role_routes import role_bp
+    from app.routes.permission_routes import perm_bp
     app.register_blueprint(user_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(role_bp)
+    app.register_blueprint(perm_bp)
 
     @app.route("/", methods=["GET"])
     def home():
         return redirect(url_for("users.get_users"))
+    
+    
+    from .models.user import User
+    from .models.permission import Permission
+    from .models.role import Role
 
     return app
